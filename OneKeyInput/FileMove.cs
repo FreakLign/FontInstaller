9576309f6ef41d1e490c87103e6e73850f59f019
FreakLign <stlign@outlook.com>
2020-05-24 18:32:07 +0800

    A

---
+++ OneKeyInput/FileMove.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Drawing.Text;
+
+namespace OneKeyInput
+{
+    class FileMove
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern int WriteProfileString(string lpszSection, string lpszKeyName, string lpszString);
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(int hWnd, // handle to destination window 
+        uint Msg, // message 
+        int wParam, // first message parameter 
+        int lParam // second message parameter 
+        );
+        [DllImport("gdi32")]
+        public static extern int AddFontResource(string lpFileName);
+        [DllImport("gdi32")]
+        public static extern int RemoveFontResource(string lpFileName);
+        public static string ReadAllFontsFile()
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            DialogResult result = folderBrowser.ShowDialog();
+            Checkcount = 0;
+            Filecount = 0;
+            if (result != DialogResult.Cancel)
+            {
+                return folderBrowser.SelectedPath.Trim();
+            }
+            return "";
+        }
+        static int Checkcount = 0;
+        public static int CheckDirectories(string FilePath)
+        {
+            
+            FileInfo[] Files = new DirectoryInfo(FilePath).GetFiles();
+            Console.WriteLine(FilePath);
+            if (Files.Length > 0)
+            {
+                foreach (FileInfo file in Files)
+                {
+                    if (file.Extension.ToLower() == ".otf" || file.Extension.ToLower() == ".ttf")
+                    {
+                        Checkcount++;
+                    }
+                }
+            }
+            DirectoryInfo[] directories = new DirectoryInfo(FilePath).GetDirectories();
+            if (directories.Length > 0)
+            {
+                foreach (DirectoryInfo directory in directories)
+                {
+                    CheckDirectories(directory.FullName);
+                }
+            }
+            return Checkcount;
+        }
+        static int Filecount = 0;
+        public static int MoveFile(string FilePath)
+        {
+            FileInfo[] Files = new DirectoryInfo(FilePath).GetFiles();
+            Console.WriteLine(FilePath);
+            if(Files.Length > 0)
+            {
+                foreach(FileInfo file in Files)
+                {
+                    if (file.Extension.ToLower() == ".otf" || file.Extension.ToLower() == ".ttf")
+                    {
+                        if(InstallFont(file) == 1) Filecount++;
+                    }
+                }
+            }
+            DirectoryInfo[] directories = new DirectoryInfo(FilePath).GetDirectories();
+            if(directories.Length > 0)
+            {
+                foreach(DirectoryInfo directory in directories)
+                {
+                    MoveFile(directory.FullName);
+                }
+            }
+            return Filecount;
+        }
+
+        private static int InstallFont(FileInfo fontPath)
+        {
+            PrivateFontCollection fontCol = new PrivateFontCollection();
+            fontCol.AddFontFile(fontPath.FullName);
+            string fontFileName = fontCol.Families[0].Name;
+            string WinFontDir = "C:\\windows\\fonts";
+            int Ret = 0;
+            int Res;
+            string targetPath;
+            const int WM_FONTCHANGE = 0x001D;
+            const int HWND_BROADCAST = 0xffff;
+            targetPath = WinFontDir + "\\" + fontFileName;
+            if (!File.Exists(targetPath))
+            {
+                File.Copy(fontPath.FullName, targetPath);
+                Ret = AddFontResource(targetPath);
+                Res = SendMessage(HWND_BROADCAST, WM_FONTCHANGE, 0, 0);
+                WriteProfileString("fonts", fontPath.Name + "(TrueType)", fontFileName);
+                return 1;
+            }
+            return Ret;
+        }
+
+    }
+}
---
+++ OneKeyInput/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+
+namespace OneKeyInput
+{
+
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("user32.dll")]
+        private static extern bool ReleaseCapture();
+        [DllImport("user32.dll")]
+        static extern bool SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+        const int WM_SYSCOMMAND = 0x0112;
+        const int SC_MOVE = 0xF010;
+        const int HTCAPTION = 0x0002;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void Exit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            IntPtr hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            ReleaseCapture();
+            SendMessage(hwnd, WM_SYSCOMMAND, SC_MOVE + HTCAPTION, 0);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            PathField.Text = FileMove.ReadAllFontsFile();
+            if(PathField.Text.Length > 0)
+            {
+                NextBtn.IsEnabled = true;
+                this.Dispatcher.Invoke(() =>
+                {
+                    int Count = FileMove.CheckDirectories(PathField.Text);
+                    var MessageQueue = SnackbarThree.MessageQueue;
+                    Task.Factory.StartNew(() => MessageQueue.Enqueue("该目录一共有" + Count + "个字体文件"));
+                });
+            }
+            else
+            {
+                NextBtn.IsEnabled = false;
+            }
+        }
+
+        private void NextBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                int Count = FileMove.MoveFile(PathField.Text);
+                var MessageQueue = SnackbarThree.MessageQueue;
+                Task.Factory.StartNew(() => MessageQueue.Enqueue("一共安装" + Count + "个字体文件"));
+            });
+        }
+    }
+}
